6139140859ae6e01844a3ba513d8ac006cd95e6b
Maikel Marshall <maikelmarshall07@gmail.com>
2026-05-29 09:21:31 -0400

    cleanup: Remove temporary test scripts

--- test-env.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-
-console.log('NOTION_API_KEY:', process.env.NOTION_API_KEY?.substring(0, 20) + '...');
-console.log('NOTION_MARKETING_DB_ID:', process.env.NOTION_MARKETING_DB_ID);
